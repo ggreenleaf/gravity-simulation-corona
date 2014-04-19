e50e38d2f23997cb8a6e56fa1a34673c03eb9b1b
Geoffrey Greenleaf <geoffrey@geoffreys-mbp.gateway.2wire.net>
2014-04-19 17:24:21 -0500

    need to fix force calculations

--- main.lua
+++ main.lua
@@ -8,11 +8,74 @@
 
 
 physics = require("physics")
-_H = display.contentHeight
-_W = display.contentWidth
-_Radius  = 20
+math = require("math")
+_CY = display.contentCenterY
+_CX = display.contentCenterX
+planetRadius  = 50
+satRadius = 5
 
-planet = display.newCircle(_W/2,_H/2,_Radius)
-physics.addBody(planet,"dynamic",{radius=_Radis,density=20.0})
-planet.gravityScale=0
 
+physics.start()
+--function definitions
+
+local function calcForceG(mOne,mTwo)
+	local dX = mOne.x - mTwo.x
+	local dY = mOne.y - mTwo.y
+
+	--forcetable for holding magnitude, x,y part of F
+	forceG = {}
+	forceG.magnitude = 0.01
+
+	forceG.distance = math.sqrt(dY^2+dX^2)	
+	local angle = math.asin(dY/forceG.distance)
+	
+
+	forceG.y = forceG.magnitude*math.sin(angle)
+	forceG.x = forceG.magnitude*math.cos(angle)
+	print(angle)
+	print(forceG.x.." "..forceG.y)
+	
+	return forceG
+end
+
+
+
+planet = display.newCircle(_CX,_CY,planetRadius)
+planetDetails = {radius=planetRadius}
+physics.addBody(planet,"static", planetDetails); planet.gravityScale = 0
+planet.mass = 100 --EarthMass
+
+
+sat = display.newCircle(_CX-150,_CY,satRadius)
+satProperties = {radius=satRadius}
+physics.addBody(sat,satProperties); sat.gravityScale = 0
+sat.mass = 1
+sat.force = calcForceG(planet,sat)
+sat:setLinearVelocity(0,-110)
+
+--mOne is oribiting mOne
+local function update(mOne, mTwo)
+	local force = calcForceG(mOne,mTwo)
+	mTwo:applyForce(force.x,force.y)
+end
+
+Runtime:addEventListener("enterFrame",
+		function()
+				update(planet,sat)
+				if (sat.x > display.contentWidth) then
+					physics.stop()
+				end
+		end)
+
+
+
+
+
+					
+
+
+
+
+
+
+--calculate the force of gravity between two objects. 
