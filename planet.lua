768329730b9f956bf4802e6c21d82ec8335d3b78
Geoffrey Greenleaf <geoffrey@geoffreys-mbp.gateway.2wire.net>
2014-04-30 08:07:26 -0500

    added planet class

---
+++ planet.lua
@@ -0,0 +1,36 @@
+
+
+
+local Planet = {}
+local planetMT = {__index = Satellite}
+
+--spawn a new satellite with center of object at (x,y), 
+--radius=r and density=d
+
+
+function Planet:new(x,y,r,m)
+	 local self = {}
+	 setmetatable(self,planetMT)
+
+
+	  --group holds planet and gravity field object
+	  self.group = display.newGroup()
+
+	 self.field = display.newCircle(x,y,r*5);  self.field.alpha = 0.3
+	 self.field.name = "field"
+	 physics.addBody(self.field,"static",{isSensor=true,radius=r*5})
+
+
+	 self.object = display.newCircle(x,y,r)
+	 physics.addBody(self.object,"static",{radius=r})
+	 self.object.mass = m
+	 self.object.gravityScale = 0
+
+
+	 return self
+end
+
+
+
+
+return Planet
